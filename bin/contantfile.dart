606f398357006ea9d6e26a6fc2ac2fb8c49e4b84
paveltech <pavelcep.sust11@gmail.com>
2020-03-19 12:31:08 +0600

    content upload

--- bin/contantfile.dart
+++ bin/contantfile.dart
@@ -6,6 +6,22 @@ void main(){
 
   print(name);
   print(Circle.link);
+  showNumber(20);
+  print(numberCheck(10, 20));
+}
+
+void showNumber(int number){
+  print('$number');
+}
+
+int number(int a , int b )=> (a+b);
+
+int numberCheck(int a , int b ){
+
+  // ignore: omit_local_variable_types
+  int number = a >b ? a : b;
+
+  return number;
 }
 
 class Circle{
