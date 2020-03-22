0dc74883f8ea90786eef70a60f0632b4f72ffb93
paveltech <pavelcep.sust11@gmail.com>
2020-03-23 01:20:59 +0600

    oop working inheritanc

--- bin/oop/Inheritance.dart
+++ bin/oop/Inheritance.dart
@@ -1,9 +1,14 @@
 
 void main(){
-  var dog = Dog('Dog ' , false);
+  var dog = Dog('Dog ' , false , 30);
   var cat = Cat('Cat' , true);
+  var cat2 = Cat.catconstrator(50);
   dog.checkEat();
+  dog.showCapasity();
+  dog.showWeight();
   cat.checkEat();
+  cat.showCapasity();
+
 }
 
 
@@ -11,13 +16,24 @@ class Animal{
   String name;
   bool isCanEat;
 
-  Animal(this.name, this.isCanEat);
+  Animal(this.name , this.isCanEat);
+
+  Animal.animalConstractor();
+
+  void showCapasity(){
+    print('${name} can a lot eat');
+  }
 
 }
 
 class Dog extends Animal{
 
-  Dog(String name, bool isCanEat) : super(name, isCanEat);
+  int weight;
+  String name;
+
+  Dog(String name, bool isCanEat, this.weight) : super(name, isCanEat);
+
+  Dog.myCustomConstractor(this.weight , this.name) : super.animalConstractor();
 
   void checkEat(){
     if(isCanEat){
@@ -26,9 +42,25 @@ class Dog extends Animal{
       print('${name} can not eat');
     }
   }
+
+  void showWeight(){
+    print('${name} weight is ${weight}');
+  }
+
+  @override
+  void showCapasity() {
+    // TODO: implement showCapasity
+    super.showCapasity();
+
+  }
 }
 
 class Cat extends Animal{
+
+  int catWeight;
+
+  Cat.catconstrator(this.catWeight): super.animalConstractor();
+
   Cat(String name, bool isCanEat) : super(name, isCanEat);
 
   void checkEat(){
@@ -37,6 +69,16 @@ class Cat extends Animal{
     }else{
       print('${name} can not eat');
     }
+  }
+
+  void showCatWeight(){
+    print('${catWeight}');
+  }
+
+  @override
+  void showCapasity() {
+    // TODO: implement showCapasity
+    super.showCapasity();
   }
 
 }
